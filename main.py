1cf29814d37e11eb8e7708d56e62f63648702e2b
Shubham Kumar <shubhamcricketer.kumar27@gmail.com>
2021-03-29 13:05:51 +0530

    minor fix 2.0

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def verify(image_path, identity, database, model):
     encoding = img_to_encoding(image_path, model)
     dist = np.linalg.norm(encoding - database[identity])
     
-    if dist < 0.6:
+    if dist < 0.4:
         print("It's " + str(identity))
         door_open = True
     else:
